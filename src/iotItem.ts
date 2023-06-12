b2b0697d382736bbbb2d9ae39749513099705967
SungYong Jang <jsy@sclab.io>
2023-06-12 17:04:59 +0900

    [iot] bug fix

--- src/iotItem.ts
+++ src/iotItem.ts
@@ -1,7 +1,6 @@
-import { DBPool, MQTT_TOPIC, QueryItem } from './config/index';
+import { DBPool, MQTT_TOPIC, QueryItem, db } from './config/index';
 import mqtt from 'mqtt';
 import { logger } from './utils/logger';
-import { PoolConnection } from 'mariadb';
 
 export class IOTItem {
   init(client: mqtt.Client, queryItem: QueryItem) {
@@ -14,22 +13,16 @@ export class IOTItem {
         return;
       }
 
-      let conn: PoolConnection;
       try {
-        conn = await DBPool.getConnection();
-        const rows = await conn.query(queryItem.query);
+        const result = await db.pool.query(queryItem.query);
         const data = JSON.stringify({
-          rows,
+          rows: result.recordset,
         });
 
         client.publish(topic, Buffer.from(data, 'utf-8'));
         logger.info(`topic: ${topic}, data: ${data}`);
       } catch (error) {
         logger.error(error);
-      } finally {
-        if (conn) {
-          conn.release();
-        }
       }
 
       setTimeout(func, queryItem.interval);
